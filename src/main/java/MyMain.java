b8fb7651a972c5145bca2186144066f9c3ec9565
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-24 13:48:40 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -8,7 +8,11 @@ public class MyMain {
     //     2
     public static void printEvensBackwards(int[] arr) {
         // REPLACE THIS WITH YOUR CODE
-        return;
+        for (int i= arr.length - 1;i >= 0; i-= 1){
+            if (arr[i] % 2 == 0){
+                System.out.println(arr[i]);
+            }
+        }
     }
 
     // Next, write a method that checks if an array is sorted in
@@ -19,7 +23,13 @@ public class MyMain {
     //     isIncreasing([1, 2, 3, 2, 4, 5, 8]) => false
     public static boolean isIncreasing(int[] arr) {
         // REPLACE THIS WITH YOUR CODE
-        return false;
+        for(int i=1;i<arr.length;i++){
+            if (arr[i] <= arr[i - 1]){
+                    return false;
+                }
+
+        }
+        return true;
     }
 
     // (Maybe Optional):
@@ -31,7 +41,12 @@ public class MyMain {
     //     isBalanced([1, 2, 3, 7, 2, 1]) => false (because 7 - 3 > 2)
     public static boolean isBalanced(int[] arr) {
         // REPLACE THIS WITH YOUR CODE
-        return false;
+        for (int i=0;i<arr.length;i++){
+            if(Math.abs(arr[i] - arr[arr.length - i - 1]) > 2){
+                return false;
+            }
+        }
+        return true;
     }
 
     // Write a method that takes an array and checks if it contains
@@ -40,6 +55,16 @@ public class MyMain {
     //     containsDuplicates([7, 2, 3, 4, 1, 5]) => false
     public static boolean containsDuplicates(int[] arr) {
         // REPLACE THIS WITH YOUR CODE
+        for (int i=0;i<arr.length;i++){
+            if (i != 0){
+                for (int j=0;j<i;j++){
+                    if (arr[j] == arr[i]){
+                        return true;
+                    }
+                }
+            }
+
+        }
         return false;
     }
 
@@ -47,9 +72,52 @@ public class MyMain {
     // Implement the bubble sort algorithm that we discussed in class
     // to sort our code
     // This algorithm returns the sorted array
+    public static int[] FindLargestNum(int[] arr){
+        int max = 0;
+        int index = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        return new int[]{max, index};
+    }
+    public static int[] PruneList(int[] arr, int index){
+        int[] newArr = new int[arr.length - 1];
+        int count = 0;
+        for(int i =0; i < arr.length; i ++){
+            if(i == index){
+                continue;
+            }
+            else{
+                newArr[count] = arr[i];
+                count ++;
+            }
+        }
+
+        return newArr;
+    }
+    public static int[] reverseList(int[] arr){
+        int[] newArr = new int[arr.length];
+        int count = 0;
+        for(int i = arr.length - 1; i >= 0; i --){
+            newArr[count] = arr[i];
+            count ++;
+        }
+        return newArr;
+    }
     public static int[] bubbleSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
-        return arr;
+        int[] finalArr = new int[arr.length];
+        int[] Arr = arr.clone();
+        int[] largestNum = new int[2];
+        for (int i = 0; i < arr.length; i ++){
+            largestNum = FindLargestNum(Arr);
+            Arr = PruneList(Arr, largestNum[1]);
+            finalArr[i] = largestNum[0];
+
+        }
+        return reverseList(finalArr);
     }
 
 
@@ -79,7 +147,7 @@ public class MyMain {
 
         System.out.println();
         System.out.println("Bubble Sort Tests");
-        System.out.println(Arrays.toString(new int[] {6, 1, 3, 8, 2, 9})); // [1, 2, 3, 6, 8, 9]
-        System.out.println(Arrays.toString(new int[] {12, 5, 23, 74, 12, 86, 2})); // [2, 5, 12, 23, 74, 86]
+        System.out.println(Arrays.toString(bubbleSort(new int[]{6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
+        System.out.println(Arrays.toString(bubbleSort(new int[]{12, 5, 23, 74, 12, 86, 2}))); // [2, 5, 12, 23, 74, 86]
     }
 }
